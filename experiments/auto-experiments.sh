c7d7d138753b19a510fafa210d39e428ab43e472
Nico <nicochico.link@gmail.com>
2020-01-22 18:57:31 +0100

    auto-experiments bug fixed

--- experiments/auto-experiments.sh
+++ experiments/auto-experiments.sh
@@ -9,7 +9,7 @@ for dir in */; do
     totalTime=0
     pointTime=0
     best=0
-    N=1 ## Number of repetitions of the test
+    N=25 ## Number of repetitions of the test
     echo "Reapeating experiment $N times -- Please wait"
     for i in $(seq 1 1 $N);do
         java -jar ../../../MaximizationProblem.jar input.txt >/dev/null
@@ -17,23 +17,26 @@ for dir in */; do
         line=$(cat solutions.txt | tail -3 | head -1)
         read -ra aux <<< "$line" 
         a=${aux[2]}
-        totalTime=$(python3 -c "print($totalTime + $a)")
+        totalTime=$(python3 -c "print( $totalTime + ($a / $N ) )")
 
         line=$(cat solutions.txt | tail -2 | head -1)
         read -ra aux <<< "$line" 
         a=${aux[2]}
-        pointTime=$(python3 -c "print($pointTime + $a)")
+        pointTime=$(python3 -c "print( $pointTime + ($a / $N ))")
 
         line=$(cat solutions.txt | tail -1 | head -1)
         read -ra aux <<< "$line" 
         a=${aux[3]}
-        best=$(python3 -c "print($best + $a)")
+        best=$(python3 -c "print( $best + ($a / $N ))")
 
     done
-    python3 -c "print($totalTime / $N)" >> results.txt
-    python3 -c "print($pointTime / $N)" >> results.txt
-    python3 -c "print($best / $N)" >> results.txt
-
+    # python3 -c "print($totalTime / $N)" >> results.txt
+    # python3 -c "print($pointTime / $N)" >> results.txt
+    # python3 -c "print($best / $N)" >> results.txt
+    echo $totalTime >> results.txt
+    echo $pointTime >> results.txt
+    echo $best >> results.txt
+    
     echo "Measured results saves in results.txt file:"
     echo "------------------------------"
     cat results.txt
