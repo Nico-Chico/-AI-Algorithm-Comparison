73259ec57ecf106fdad968e921a6e486453144ca
Nico <nicochico.link@gmail.com>
2020-01-22 18:09:59 +0100

    Adding final features and testing tools

--- experiments/auto-experiments.sh
+++ experiments/auto-experiments.sh
@@ -1,8 +1,42 @@
-
+#!/bin/bash
 ## Begin on experiments folder
-expN = 1
+cd exp1         # SELECT EXPERIMENT
 
-cd experiment-1
-cd exp1a ## for para todas las carpetas que halla
+for dir in */; do
+    echo "EXPERIMENT $dir"
+    cd $dir
+    rm results.txt    
+    totalTime=0
+    pointTime=0
+    best=0
+    N=1 ## Number of repetitions of the test
+    echo "Reapeating experiment $N times -- Please wait"
+    for i in $(seq 1 1 $N);do
+        java -jar ../../../MaximizationProblem.jar input.txt >/dev/null
 
-java -jar ../../MaximizationProblem.jar input.txt
+        line=$(cat solutions.txt | tail -3 | head -1)
+        read -ra aux <<< "$line" 
+        a=${aux[2]}
+        totalTime=$(python3 -c "print($totalTime + $a)")
+
+        line=$(cat solutions.txt | tail -2 | head -1)
+        read -ra aux <<< "$line" 
+        a=${aux[2]}
+        pointTime=$(python3 -c "print($pointTime + $a)")
+
+        line=$(cat solutions.txt | tail -1 | head -1)
+        read -ra aux <<< "$line" 
+        a=${aux[3]}
+        best=$(python3 -c "print($best + $a)")
+
+    done
+    python3 -c "print($totalTime / $N)" >> results.txt
+    python3 -c "print($pointTime / $N)" >> results.txt
+    python3 -c "print($best / $N)" >> results.txt
+
+    echo "Measured results saves in results.txt file:"
+    echo "------------------------------"
+    cat results.txt
+    echo ""
+    cd ..
+done
