977ea807b635122c869d25e1ad87ec576048d024
Nico <nicochico.link@gmail.com>
2020-01-23 19:17:47 +0100

    Script fixed  again

--- experiments/auto-experiments.py
+++ experiments/auto-experiments.py
@@ -15,34 +15,43 @@ for i in folders:
     totalTime=0.0
     pointTime=0.0
     solution=0.0
-    N=20     ## Number of repetitions of the test
+    N=50    ## Number of repetitions of the test
     print("Repeating experiment "+str(N)+" times -- Please wait") 
-    for i in range(1, N):
+    for i in range(0, N):
         os.system("java -jar ../../../MaximizationProblem.jar input.txt >/dev/null")
+        print('.', end='', flush=True)
+        
         os.system("tail -n3 solutions.txt > values.txt")
         # os.system("cat values.txt") # To see all values are OK!
         fp = open('values.txt', 'r')
         line = fp.readline().replace('\n', '') #Removing newline
         s = line.split(': ')
         aux = s[1]
-        totalTime = totalTime + (float(aux) / N);
+        # totalTime = totalTime + (float(aux) / N);
+        totalTime = totalTime + float(aux);
         
         line = fp.readline().replace('\n', '') #Removing newline
         s = line.split(': ')
         aux = s[1]
-        pointTime = pointTime + (float(aux) / N);
+        # pointTime = pointTime + (float(aux) / N);
+        pointTime = pointTime + float(aux);
         
         line = fp.readline().replace('\n', '') #Removing newline
         line = line.split(': ')
         sol = line[1].split()
         aux = float(sol[2])
-        solution = solution + (float(aux) / N);
+        # solution = solution + (float(aux) / N);
+        solution = solution + float(aux);
+    
+    totalTime = totalTime / N
+    pointTime = pointTime / N
+    solution = solution / N
     
     os.system("echo "+ str(totalTime) +" >> results.txt")
     os.system("echo "+ str(pointTime) +" >> results.txt")
     os.system("echo "+ str(solution) +" >> results.txt")
     
-    print("Measured results saves in results.txt file:")
+    print("\nMeasured results saves in results.txt file:")
     print("------------------------------")
     os.system("cat results.txt")
     print("")
